00c2f781ce6f9a8fda19d808290edbe231470d5b
John Drogo <droatsyr@gmail.com>
2016-08-11 20:14:35 -0400

    Fix reddit parsing in case error is returned/data is unavailable

--- public/js/parsers/reddit-parser.js
+++ public/js/parsers/reddit-parser.js
@@ -16,18 +16,26 @@ const redditParser = (chartName, chartData, callback) => {
 
     //This is the data for the chart.
     for (let dataPoint of chartData){
+        let isThreeDaysOld = undefined
+        if (dataPoint.data !== undefined){
+            isThreeDaysOld = dataPoint.data.children.reduce((p, c) =>
+                moment(c.data.created_utc*1000).isAfter(moment(dataPoint.date).subtract(3, 'days')) ? p+1 : p, 0)
+        }
         dataset.datasets[0].data.push({
             x: dataPoint.date,
-            y: dataPoint.data.children.reduce((p, c) =>
-                moment(c.data.created_utc*1000).isAfter(moment(dataPoint.date).subtract(3, 'days')) ? p+1 : p, 0)
+            y: isThreeDaysOld
         })
     }
 
     //This is the data for the overview bar.
     const latestData = chartData[chartData.length-1] || {}
-    dataset.currentData = {
-        "posts (past 72 hours)": latestData.data.children.reduce((p, c) => 
+    let postsLastThreeDays = undefined
+    if (latestData.data){
+        postsLastThreeDays = latestData.data.children.reduce((p, c) => 
             moment(c.data.created_utc*1000).isAfter(moment().subtract(3, 'days')) ? p+1 : p, 0)
+    }
+    dataset.currentData = {
+        "posts (past 72 hours)": postsLastThreeDays 
     }
 
     let chartStyle = JSON.parse(JSON.stringify(defaultChartOptions))
